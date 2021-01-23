dccd42ba90b4c47a7bad73f306aeb6fd0a4e2cc2
Nicola Destro <nicola.destro.cap@gmail.com>
2021-01-23 18:22:05 +0100

    adjustments and difference computation

--- Class_ex_1_02_data_preparation.R
+++ Class_ex_1_02_data_preparation.R
@@ -12,3 +12,14 @@ rec_prlm_df$AGE_NUM<-as.numeric(rec_prlm_df$AGE)
 #computing average age for each country
 group_prlm_df<-rec_prlm_df %>% group_by(COUNTRY) %>% summarise(avg_age = mean(AGE_NUM, na.rm = T))
 View(group_prlm_df)
+
+View(rec_prlm_df)
+
+#creating the median age of population variable
+ppl_mdn_df<- data.frame(COUNTRY = c("Italy", "Netherlands", "Iceland"), ppl_mdn = c(45.5, 42.6, 36.5))
+
+#merging the two dataframes
+age_cmprd <- merge(group_prlm_df,ppl_mdn_df,by="COUNTRY")
+
+#calculating the difference between average age of PMs and median age of the population
+age_cmprd<-mutate(age_cmprd, diff_age = avg_age- ppl_mdn)
