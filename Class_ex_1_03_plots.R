f53fb00d6a8fb09264d21aa35cd360c90efe394d
Nicola Destro <nicola.destro.cap@gmail.com>
2021-01-24 19:48:26 +0100

    end this

--- Class_ex_1_03_plots.R
+++ Class_ex_1_03_plots.R
@@ -5,22 +5,19 @@ library(tidyr)
 library(tidyverse)
 
 #renaming vars for the plot
-age_cmprd<- rename(age_cmprd, "age of PMs (avg)" = "average age of PMs")
+age_cmprd<- rename(age_cmprd, "age of PMs (avg)" = "avg_age")
 age_cmprd<- rename( age_cmprd, "age of population (median)" = ppl_mdn)
 age_cmprd<- rename(age_cmprd, "difference" = diff_age)
 
 
 #plotting a bar graph of our comparison
 age_cmprd %>%
-  gather("Type", "Value",-COUNTRY) %>%
-  ggplot(aes(COUNTRY, Value, fill = Type))+
+  gather("Type", "Values",-COUNTRY) %>%
+  ggplot(aes(COUNTRY, Values, fill = Type))+
   geom_bar(position = "dodge", stat = "identity")+
   labs(title = "Comparison between PMs average age and median age of population")+
 ylab("AGE")+
   scale_fill_manual(values = c("aquamarine", "dark blue","orange"), "")+
   theme_bw()
 
-#finished
-  
-  
 
